9514e796445b86cad7de0efd58620c55b47974cc
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-23 18:16:47 +0000

    added a message once connection is established as well as a Name message to let the other players know who we are in the game once we appear

--- client.js
+++ client.js
@@ -3,15 +3,19 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: '192.168.0.103',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
   conn.on('data', (data) => {
     console.log('Server says: ', data);
+  });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server")
+    conn.write("Name: SRG")
   });
   return conn;
 }
